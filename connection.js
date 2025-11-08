d4c90034ac76ebd6528d07fc844d9252fe5c33d5
arifzain74 <arif.zain74@yahoo.com>
2025-11-08 14:38:00 +0530

    atlas-updated

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function connection(){
-    const db = await mongoose.connect('mongodb+srv://arifzain74_db_user:n2HMZCiWc9o1H4FJ@cluster0.dnnl1z5.mongodb.net//BackendExpress')
+    const db = await mongoose.connect('mongodb+srv://arifzain74_db_user:n2HMZCiWc9o1H4FJ@cluster0.dnnl1z5.mongodb.net/BackendExpress')
     console.log("database connected");
     
     return db
